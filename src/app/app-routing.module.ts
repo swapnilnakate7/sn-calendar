d8dce79d5921f1f7111ae362d5820fc1f2e02e31
Swapnil Nakate <nakate.swapnil7@gmail.com>
2019-02-27 01:01:18 +0530

    updated bug fix minor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-//import {SnCalendarComponent} from '../../projects/sn-calendar-lib/src/public_api';
-import {SnCalendarComponent} from '../../dist/sn-calendar';
+import {SnCalendarComponent} from '../../projects/sn-calendar-lib/src/public_api';
+//import {SnCalendarComponent} from '../../dist/sn-calendar';
 
 const routes: Routes = [{
   path: '', component: SnCalendarComponent, pathMatch: 'full'
