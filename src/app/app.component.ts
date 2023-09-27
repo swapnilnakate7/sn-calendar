881168e77114f1299399ecfb3c5695fc5a878b3d
Swapnil Nakate <nakate.swapnil7@gmail.com>
2023-09-27 21:40:22 +0530

    updated package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,29 +4,28 @@
  * Twitter: @SwapnilNakate7 Email:nakate.swapnil7@gmail.com
  * [***DO NOT REMOVE THIS***]
  */
-import { Component } from '@angular/core';
-import * as moment from 'moment';
-import { Moment } from 'moment';
+import { Component } from "@angular/core";
+import moment from "moment";
+import { Moment } from "moment";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-
-  title = 'sn-calendar v0.1.4';
+  title = "sn-calendar v0.1.4";
   date = moment();
-  dateRangeSelected?:any;
-  locale = 'fr';
+  dateRangeSelected?: any;
+  locale = "fr";
   restrictPast = true;
 
   getUpdatedDate(e: Moment) {
     this.date = e;
   }
 
-  showDateRange(e:any){
-    console.log('showDateRange',e);
+  showDateRange(e: any) {
+    console.log("showDateRange", e);
     this.dateRangeSelected = e;
   }
 }
